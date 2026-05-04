d9db75aea33ccdf5f69fa101441e4c93889e4c5b
kakadacancer-code <kakadacancer@gmail.com>
2026-05-04 14:59:53 +0700

    change relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -21,7 +21,7 @@ class Task extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasMany(User::class);
     }
 
     public function priority()
